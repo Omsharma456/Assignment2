19015889ec66b6ed791b73d6ce92be102729656d
Prashant18290 <Prashant.dubey_cs18@gla.ac.in>
2020-03-31 13:53:06 +0545

    All of the code written

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -26,6 +26,42 @@ public class MyBinarySearchTree {
         stringBuilder.append(root.getKey() + " ");
         preOrder(root.getLeftChild());
         preOrder(root.getRightChild());
+    }
 
+    public static void postOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.getLeftChild());
+        postOrder(root.getRightChild());
+        stringBuilder.append(root.getKey() + " ");
+    }
+
+    public static void inOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.getLeftChild());
+        stringBuilder.append(root.getKey() + " ");
+        inOrder(root.getRightChild());
+    }
+
+    public void add(final int value) {
+        this.root = addService(this.root, value);
+    }
+
+    public TreeNode addService(final TreeNode treeRoot, final int addingValue) {
+
+        if (treeRoot == null) {
+            return new TreeNode(addingValue);
+        }
+        if (addingValue < treeRoot.getKey()) {
+            treeRoot.setLeftChild(addService(treeRoot.getLeftChild(), addingValue));
+        } else if (addingValue == treeRoot.getKey()) {
+            return treeRoot;
+        } else {
+            treeRoot.setRightChild(addService(treeRoot.getRightChild(), addingValue));
+        }
+        return treeRoot;
     }
 }
