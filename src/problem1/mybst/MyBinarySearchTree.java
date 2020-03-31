aaed9e34830a059817c91b5a2d5714e4d80c87c0
Prashant18290 <Prashant.dubey_cs18@gla.ac.in>
2020-03-31 13:41:47 +0545

    TreeNode

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,8 +5,12 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
     public static StringBuilder stringBuilder;
     public int levels = 0;
+    public TreeNode root;
 }
